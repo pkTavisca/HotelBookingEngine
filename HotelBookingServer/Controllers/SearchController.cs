5725e2606cf453b5bdb489bd9911dc76506105ac
Pratik Kumar <pkumar@tavisca.com>
2017-10-04 14:03:03 +0530

    Search controller added

---
+++ HotelBookingServer/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingServer.Controllers
+{
+    [Route("/api/[controller]")]
+    public class SearchController : Controller
+    {
+        [HttpPost("new")]
+        public string OnNewSearch()
+        {
+            return "some id";
+        }
+    }
+}
