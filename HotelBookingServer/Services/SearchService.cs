e55b2694f8bd9547d48f86ff5d0e02c8d8a62501
Pratik Kumar <pkumar@tavisca.com>
2017-10-05 11:22:37 +0530

    Reading config from appsettings

--- HotelBookingServer/Services/SearchService.cs
+++ HotelBookingServer/Services/SearchService.cs
@@ -3,18 +3,32 @@ using HotelBookingServer.Constants;
 using HotelBookingServer.Contracts;
 using HotelBookingServer.Generators;
 using HotelBookingServer.Models;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace HotelBookingServer.Services
 {
     public class SearchService
     {
         private ISearchCache _searchCache = SearchCacheGenerator.Generate(CacheType.InMemory);
-        private object _searchResultsCache = 
+        private ISearchAutoSuggestCache _searchAutoSuggestResultsCache = SearchAutosuggestCacheGenerator.Generate(CacheType.InMemory);
+        private AppSettings _appSettings;
+
+        public SearchService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
 
         public string OnNewSearch(SearchObject searchObject)
         {
             string searchGuid = _searchCache.AddToCache(searchObject);
+            Task.Factory.StartNew(() => AddResultsToCache(searchObject, searchGuid));
             return searchGuid;
+        }
+
+        private void AddResultsToCache(SearchObject searchObject, string searchGuid)
+        {
+            string autosuggestApiUrl = _appSettings.SearchAutosuggestApiBaseUrl + searchObject.SearchTerm;
         }
 
         public SearchObject GetSearchObject(string searchId)
@@ -22,9 +36,9 @@ namespace HotelBookingServer.Services
             return _searchCache.GetFromCache(searchId);
         }
 
-        public object GetResults(string searchId)
+        public object GetAutoSuggestResults(string searchId)
         {
-
+            return null;
         }
     }
 }
--- HotelBookingServer/Startup.cs
+++ HotelBookingServer/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using HotelBookingServer.Models;
 
 namespace HotelBookingServer
 {
@@ -29,6 +30,8 @@ namespace HotelBookingServer
         {
             // Add framework services.
             services.AddMvc();
+            var appSettings = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettings);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
