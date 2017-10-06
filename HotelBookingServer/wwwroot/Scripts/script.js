1632d2195f81c2aa148aca75f17821c6a88e5606
subham <ssaraf@tavisca.com>
2017-10-06 11:17:02 +0530

    Added the ajax autosuggest call in the alert

--- HotelBookingServer/wwwroot/Scripts/script.js
+++ HotelBookingServer/wwwroot/Scripts/script.js
@@ -1,1 +1,45 @@
-﻿
+﻿$("button").click(function () {
+    var term = $("#search-field")[0].value;
+    var ci = $("#ci-datepicker")[0].value;
+    var co = $("#co-datepicker2")[0].value;
+    var data = {
+        "searchterm": term,
+        "checkin": ci,
+        "checkout": co
+    }
+    var datam = JSON.stringify(data);
+    $.ajax({
+        url: '/api/search/new',
+        type: 'post',
+        contentType: "application/json",
+        success: function (result) {
+            alert(result);
+        },
+        data: datam
+    });
+});
+
+
+var jsonobject;
+$("#search-field").keyup(function () {
+    var search = $("#search-field")[0].value;
+    $.ajax({
+        url: '/api/search/getAutoSuggestResults/' + search,
+        type: 'get',
+        contentType: "application/json",
+        success: function (jsonresult) {
+
+            alert(parseit(jsonresult));
+
+        }
+    })
+})
+function parseit(jsonresult) {
+    var ooo
+    var outputf;
+    jsonobject = JSON.parse(jsonresult);
+    for (var i = 0; i < jsonobject[0].ItemList.length; i++) {
+        ooo += jsonobject[0].ItemList[i].Name + "\n";
+    }
+    return ooo;
+}
