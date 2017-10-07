8dc0edcab6f9d9522fdcb6f0b2b6a0609ea628c9
Pratik Kumar <pkumar@tavisca.com>
2017-10-07 17:36:22 +0530

    base page for hotel listing

---
+++ HotelBookingServer/wwwroot/Scripts/hotel-listing-script.js
@@ -0,0 +1,14 @@
+﻿$.ajax({
+    type: "GET",
+    url: 'api/hotel/get',
+    success: onSuccess
+});
+
+function onSuccess(result) {
+    var hotelListHtml = "";
+    for (itinerary of result.itineraries) {
+        if (itinerary.itineraryType !== "Hotel") continue;
+        hotelListHtml += "<div>" + itinerary.hotelProperty.name + "</div>";
+    }
+    $("#hotel-list").html(hotelListHtml);
+}
