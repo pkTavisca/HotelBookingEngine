b263f5e73d87b8ebe9fce6994d8ec6f73c582bdb
subham <ssaraf@tavisca.com>
2017-10-08 21:18:01 +0530

    direct connect to single avail, if search field selected is hotel

--- HotelBookingServer/wwwroot/Scripts/hotel-listing-script.js
+++ HotelBookingServer/wwwroot/Scripts/hotel-listing-script.js
@@ -6,7 +6,7 @@ var searchResults;
 $.ajax({
     type: "GET",
     url: '../api/search/get/' + guid,
-    success: function(result) {
+    success: function (result) {
         searchResults = JSON.parse(result.searchTerm).item.data;
         hotelAjaxCall();
     }
@@ -23,7 +23,13 @@ function hotelAjaxCall() {
 function onSuccess(result) {
     $("#loading-icon").hide();
     var hotelListHtml = "";
+    var isHotel = false;
+    if (searchResults.SearchType === "Hotel" || searchResults.SearchType === "hotel")
+        isHotel = true;
+    var i = 0;
     for (itinerary of result.itineraries) {
+        i++;
+        if (isHotel && i > 1) continue;
         if (itinerary.itineraryType !== "Hotel") continue;
         hotelListHtml += "<div>" + itinerary.hotelProperty.name + "</div>";
     }
