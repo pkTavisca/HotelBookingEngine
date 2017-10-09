5b580a8e3921ad373e0fd6c6a9ad5651bcf657ab
subham <ssaraf@tavisca.com>
2017-10-09 15:30:12 +0530

    Completed the parsing of the single avail HotelRoomAvailRQ

--- HotelBookingServer/Services/SingleAvailService.cs
+++ HotelBookingServer/Services/SingleAvailService.cs
@@ -135,10 +135,75 @@ namespace HotelBookingServer.Services
                 IsReturnRooms = false,
                 Location = new Location()
                 {
-                   
+                   CodeContext = LocationCodeContext.City,
+                   GeoCode = new GeoCode()
+                   {
+                       Latitude = 36.11093f,
+                       Longitude = -115.16935f
+                   },
+                   GmtOffsetMinutes = 0,
+                   Id = 0,
+                   Name = "Las Vegas",
+                   Radius = new Distance
+                   {
+                       Amount = 30,
+                       From = LocationCodeContext.City,
+                       Unit = DistanceUnit.mi
+                   },
+                },
+                NoOfRooms = 1,
+                ProcessingInfo = new HotelSearchProcessingInfo()
+                {
+                    DisplayOrder = HotelDisplayOrder.ByPriceLowToHigh
+                },
+                RoomOccupancyTypes = new RoomOccupancyType[]
+                {
+                    new RoomOccupancyType()
+                    {
+                        PaxQuantities = new PassengerTypeQuantity[]
+                        {
+                            new PassengerTypeQuantity()
+                            {
+                                Ages = new int[]
+                                {
+                                    30,
+                                    30
+                                },
+                                PassengerType = PassengerType.Adult,
+                                Quantity = 2
+                            }
+                        }
+                    }
+                },
+                SearchType = HotelSearchType.City,
+                StayPeriod = new DateTimeSpan()
+                {
+                    Duration = 0,
+                    End = new DateTime(2017,10,26),
+                    Start = new DateTime(2017,10,25)
+                },
+                TravelPreference = new HotelSearchPreference()
+                {
+                    MaxNumberOfBedRooms = 0,
+                    MaxOccupancy = 0,
+                    MinNumberOfBedRooms = 0,
+                    MinOccupancy = 0
                 }
                 
-            }
+            },
+            Itinerary = new HotelItinerary()
+            {
+                Id = new Guid("00000000-0000-0000-0000-000000000000"),
+                ItineraryStatus = ItineraryStatusType.Unbooked,
+                Rph = 0,
+                AllPaxDetailsRequired = false,
+                GuaranteeRequired = false,
+                HotelProperty = new HotelProperty()
+                {
+                    Id = 88359
+                },
+                ShippingAddressRequired = false
+            }       
         };
     }
 }
