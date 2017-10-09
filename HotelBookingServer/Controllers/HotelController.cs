e44bfd34bd86fecf5454849f3f44484a0a738358
subham <ssaraf@tavisca.com>
2017-10-09 15:57:08 +0530

    Response coming, HotelRoomAvailRS, Hardcoded

--- HotelBookingServer/Controllers/HotelController.cs
+++ HotelBookingServer/Controllers/HotelController.cs
@@ -1,9 +1,8 @@
 ﻿using HotelBookingServer.Models;
-using hotelRef =  HotelEngineServiceReference;
+using HotelEngineServiceReference;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using HotelBookingServer.Services;
-using tripRef = TripEngineServiceReference;
 
 namespace HotelBookingServer.Controllers
 {
@@ -12,24 +11,26 @@ namespace HotelBookingServer.Controllers
     {
         private AppSettings _appSettings;
         private HotelService _hotelService;
+        private SingleAvailService _singleAvail;
 
         public HotelController(IOptions<AppSettings> appSettings)
         {
             _appSettings = appSettings.Value;
             _hotelService = new HotelService(_appSettings);
+            _singleAvail = new SingleAvailService();
         }
 
         [HttpGet("get/{type}/{lat}/{lon}")]
-        public hotelRef.HotelSearchRS GetHotelDetails(string type, string lat, string lon)
+        public HotelSearchRS GetHotelDetails(string type, string lat, string lon)
         {
             return _hotelService.GetHotelDetails(type, lat, lon);
         }
 
 
         [HttpGet("single")]
-        public tripRef.HotelRoomAvailRS Get()
+        public HotelRoomAvailRS Get()
         {
-            return null;
+            return _singleAvail.GetHotel();
         }
     }
 }
