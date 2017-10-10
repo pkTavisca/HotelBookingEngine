642cf4094798c458ef4906d16cf8afe75cfc9f91
Pratik Kumar <pkumar@tavisca.com>
2017-10-10 12:24:06 +0530

    Added ajax call in hotel details page

---
+++ HotelBookingServer/wwwroot/Scripts/hotel-details.js
@@ -0,0 +1,17 @@
+﻿var currentUrl = window.location.href;
+var lastIndex = currentUrl.lastIndexOf("/");
+var hotelId = currentUrl.slice(lastIndex + 1);
+
+function hotelAjaxCall() {
+    $.ajax({
+        type: "GET",
+        url: '../api/hotel/single/' + hotelId,
+        success: onSuccess
+    });
+}
+
+function onSuccess(result) {
+    console.log(result);
+}
+
+hotelAjaxCall();
