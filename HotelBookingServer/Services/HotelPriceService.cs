57af208c049b21bfe7ac76d2fd1199af9a38b32a
Pratik Kumar <pkumar@tavisca.com>
2017-10-11 20:04:04 +0530

    Added cache and session id retrieval of RoomAvailRQ and RS

--- HotelBookingServer/Services/HotelPriceService.cs
+++ HotelBookingServer/Services/HotelPriceService.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HotelEngineServiceReference;
+using HotelBookingServer.Implementations;
 
 namespace HotelBookingServer.Services
 {
     public class HotelPriceService
     {
-        public HotelRoomPriceRS GetPrice()
+        public HotelRoomPriceRS GetPrice(string sessionId)
         {
+            var hotelRoomAvailTuple = HotelRoomAvailRQRSCache.GetFromCache(sessionId);
             HotelEngineClient hotelEngineClient = new HotelEngineClient();
             HotelRoomPriceRQ priceRequest = new HotelRoomPriceRQ()
             {
                 AdditionalInfo = new StateBag[] { },
-                HotelSearchCriterion = new HotelSearchCriterion() { },
-                Itinerary = new HotelItinerary() { },
+                HotelSearchCriterion = hotelRoomAvailTuple.Item1.HotelSearchCriterion,
+                Itinerary = hotelRoomAvailTuple.Item2.Itinerary,//new HotelItinerary() { },
                 ResultRequested = ResponseType.Complete,
-                SessionId = ""
+                SessionId = sessionId
             };
             HotelRoomPriceRS priceResponse = hotelEngineClient.HotelRoomPriceAsync(priceRequest).GetAwaiter().GetResult();
             return priceResponse;
