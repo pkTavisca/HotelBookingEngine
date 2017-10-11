1d4a16aadf7a3b74a12b5fa499a0a36ccd3237cd
Pratik Kumar <pkumar@tavisca.com>
2017-10-11 19:38:27 +0530

    Base for sending the pricing request

---
+++ HotelBookingServer/Controllers/RoomPriceController.cs
@@ -0,0 +1,23 @@
+﻿using HotelBookingServer.Services;
+using HotelEngineServiceReference;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingServer.Controllers
+{
+    [Route("/api/[controller]")]
+    public class RoomPriceController : Controller
+    {
+        HotelPriceService _hotelPriceService;
+
+        public RoomPriceController()
+        {
+            _hotelPriceService = new HotelPriceService();
+        }
+
+        [HttpGet("get")]
+        public HotelRoomPriceRS GetPrice()
+        {
+            return _hotelPriceService.GetPrice();
+        }
+    }
+}
---
+++ HotelBookingServer/Services/HotelPriceService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelEngineServiceReference;
+
+namespace HotelBookingServer.Services
+{
+    public class HotelPriceService
+    {
+        public HotelRoomPriceRS GetPrice()
+        {
+            HotelEngineClient hotelEngineClient = new HotelEngineClient();
+            HotelRoomPriceRQ priceRequest = new HotelRoomPriceRQ()
+            {
+                AdditionalInfo = new StateBag[] { },
+                HotelSearchCriterion = new HotelSearchCriterion() { },
+                Itinerary = new HotelItinerary() { },
+                ResultRequested = ResponseType.Complete,
+                SessionId = ""
+            };
+            HotelRoomPriceRS priceResponse = hotelEngineClient.HotelRoomPriceAsync(priceRequest).GetAwaiter().GetResult();
+            return priceResponse;
+        }
+    }
+}
