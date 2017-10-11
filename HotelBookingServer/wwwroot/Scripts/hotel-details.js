6032d28f82ffca5ecc14a046ae9a1cf1bf5da0a7
Sakshi Sharma <sakshis@tavisca.com>
2017-10-11 15:36:57 +0530

    Containers made for hotel-info

--- HotelBookingServer/wwwroot/Scripts/hotel-details.js
+++ HotelBookingServer/wwwroot/Scripts/hotel-details.js
@@ -2,6 +2,14 @@
 var lastIndex = currentUrl.lastIndexOf("/");
 var hotelId = currentUrl.slice(lastIndex + 1);
 
+$.ajax({
+    type: "GET",
+    url: '../api/search/get/' + hotelId,
+    success: function (result) {
+        searchResults = JSON.parse(result.searchTerm).item.data;
+        hotelAjaxCall();
+    }
+});
 function hotelAjaxCall() {
     $.ajax({
         type: "GET",
@@ -13,9 +21,11 @@ function hotelAjaxCall() {
 function onSuccess(result) {
     console.log(result);
     $("#loading-icon").hide();
-    
+    if (searchResults.SearchType === "Hotel" || searchResults.SearchType === "hotel")
+        isHotel = true;
+    var i = 0;
+    var htmlContainer = $('#single-hotel-list');
+    htmlContainer.addClass('hotelinfo-div');
+
     }
-
-    hotelAjaxCall();
-
 
