8ad3d43e63eaf7b7725474294fb9a4ef70d8180c
Sakshi Sharma <sakshis@tavisca.com>
2017-10-11 16:34:44 +0530

    Minute changes

--- HotelBookingServer/wwwroot/Scripts/hotel-details.js
+++ HotelBookingServer/wwwroot/Scripts/hotel-details.js
@@ -1,15 +1,8 @@
 ﻿var currentUrl = window.location.href;
 var lastIndex = currentUrl.lastIndexOf("/");
 var hotelId = currentUrl.slice(lastIndex + 1);
+var searchResults;
 
-$.ajax({
-    type: "GET",
-    url: '../api/search/get/' + hotelId,
-    success: function (result) {
-        searchResults = JSON.parse(result.searchTerm).item.data;
-        hotelAjaxCall();
-    }
-});
 function hotelAjaxCall() {
     $.ajax({
         type: "GET",
@@ -17,15 +10,12 @@ function hotelAjaxCall() {
         success: onSuccess
     });
 }
+hotelAjaxCall();
 
 function onSuccess(result) {
     console.log(result);
-    $("#loading-icon").hide();
-    if (searchResults.SearchType === "Hotel" || searchResults.SearchType === "hotel")
-        isHotel = true;
-    var i = 0;
-    var htmlContainer = $('#single-hotel-list');
-    htmlContainer.addClass('hotelinfo-div');
+    
+
 
     }
 
