ba2eeb3cbf4380eab3b30b09fed0a7a90dbebeac
subham <ssaraf@tavisca.com>
2017-10-30 18:55:58 +0530

    conformation.JS values addded from the object created, need to change the session storage . get Items from the Payment Index

--- HotelBookingServer/wwwroot/Scripts/Conformation.js
+++ HotelBookingServer/wwwroot/Scripts/Conformation.js
@@ -6,15 +6,20 @@
     var inDate = confirmationDetails.checkinDate;
     var outDate = confirmationDetails.checkoutDate;
     var htmlData = {
-        ConfirmationId: " "+confirmationDetails.
-        hotelName: " " + confirmationDetails.hotelName,
-        roomName: " " + confirmationDetails.roomName,
-        checkInDate: " " + inDate,
-        checkOutDate: " " + outDate,
-        nightsOfStay: " " + confirmationDetails.nights
+        ConfirmationId: " " + confirmationDetails.ConfirmationID,
+        hotelName: " " + confirmationDetails.HotelName,
+        roomName: " " + confirmationDetails.RoomName,
+        checkInDate: " " + confirmationDetails.CheckIn,
+        checkOutDate: " " + confirmationDetails.CheckOut,
+        nightsOfStay: " " + confirmationDetails.NoOfNights,
+        email: " " + confirmationDetails.Email,
+        address: " " + confirmationDetails.Address
+
         //amount: " " + (" " + confirmationDetails.status)
     }
     $("#hotelName").val(htmlData.hotelName);
+    $("#email").val(htmlData.email);
+    $("#address").val(htmlData.address);
     $("#roomName").val(htmlData.roomName);
     $("#checkIn").val(htmlData.checkInDate);
     $("#checkOut").val(htmlData.checkOutDate);
