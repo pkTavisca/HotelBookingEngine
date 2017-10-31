ef1703023ae1f8f1a4a15282fe5773fe450bb2a5
Pratik Kumar <pkumar@tavisca.com>
2017-10-31 10:28:32 +0530

    ReferenceConverter Test

---
+++ Cache.Tests/ReferenceConverterFixture.cs
@@ -0,0 +1,28 @@
+﻿using HotelBookingServer.Implementations;
+using HotelBookingServer.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cache.Tests
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+    }
+
+    [TestClass]
+    public class ReferenceConverterFixture
+    {
+        [TestMethod]
+        public void Check_If_Object_Type_Converter_Is_Working_Or_Not()
+        {
+            Person person = new Person() { Name = "abc" };
+            Student converted = ReferenceConverter.Convert<Person, Student>(person);
+            Assert.AreEqual("abc", converted.Name);
+        }
+    }
+}
